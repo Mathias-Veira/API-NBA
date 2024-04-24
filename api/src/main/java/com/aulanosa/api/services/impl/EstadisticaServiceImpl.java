3fbbb30d4d73cfcc967f31ac1b0e4e561f7d7e81
Mathias-Veira <mveirad@gmail.com>
2024-04-24 19:12:46 +0200

    jugadores y promedios update

--- api/src/main/java/com/aulanosa/api/services/impl/EstadisticaServiceImpl.java
+++ api/src/main/java/com/aulanosa/api/services/impl/EstadisticaServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Service;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -33,69 +34,88 @@ public class EstadisticaServiceImpl implements EstadisticaService {
     private EstadisticaRepository estadisticaRepository;
     @Autowired
     private JugadorService jugadorService;
-    private List<EstadisticaDTO> obtenerPromedios(){
+
+    private List<EstadisticaDTO> obtenerPromedios() {
         List<EstadisticaDTO> promedios = new ArrayList<>();
         List<JugadorDTO> jugadores = jugadorService.obtenerJugadoresTemporada();
         int[] playerIds = jugadores.stream().mapToInt(JugadorDTO::getIdJugador).toArray();
 
-        String playerIdParams = IntStream.of(playerIds)
+
+        int midpoint = playerIds.length / 2;
+        int[] firstHalf = Arrays.copyOfRange(playerIds, 0, midpoint);
+        int[] secondHalf = Arrays.copyOfRange(playerIds, midpoint, playerIds.length);
+
+        String firstHalfParams = IntStream.of(firstHalf)
+                .mapToObj(id -> "player_ids[]=" + id)
+                .collect(Collectors.joining("&"));
+
+        String secondHalfParams = IntStream.of(secondHalf)
                 .mapToObj(id -> "player_ids[]=" + id)
                 .collect(Collectors.joining("&"));
 
         try {
-            URL url = new URL("https://api.balldontlie.io/v1/season_averages?season=2023&" + playerIdParams);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty("Authorization", System.getenv("BALL_DONT_LIE"));
-            connection.setRequestMethod("GET");
-
-            connection.connect();
-            int code = connection.getResponseCode();
-
-            if (code != 200) {
-                System.out.println("Se produjo un error: " + code);
-                System.out.println(connection.getResponseMessage());
-            } else {
-                StringBuilder information = new StringBuilder();
-                Scanner sc = new Scanner(connection.getInputStream());
-
-                while (sc.hasNext()) {
-                    information.append(sc.nextLine());
+            for (int i = 0; i < 2; i++) {
+                URL url = null;
+                if(i==0){
+                    url = new URL("https://api.balldontlie.io/v1/season_averages?season=2023&" + firstHalfParams);
+                }else{
+                    url = new URL("https://api.balldontlie.io/v1/season_averages?season=2023&" + secondHalfParams);
                 }
 
-                sc.close();
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestProperty("Authorization", System.getenv("BALL_DONT_LIE"));
+                connection.setRequestMethod("GET");
 
-                JSONArray jsonArray = new JSONObject(information.toString()).getJSONArray("data");
+                connection.connect();
+                int code = connection.getResponseCode();
 
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    EstadisticaDTO estadisticaDTO = new EstadisticaDTO();
-                    estadisticaDTO.setIdJugador(jsonObject.getInt("player_id"));
-                    estadisticaDTO.setPuntosPorPartido(jsonObject.getDouble("pts"));
-                    estadisticaDTO.setAsistenciasPorPartido(jsonObject.getDouble("ast"));
-                    estadisticaDTO.setPerdidasPorPartido(jsonObject.getDouble("turnover"));
-                    estadisticaDTO.setFaltasPorPartido(jsonObject.getDouble("pf"));
-                    estadisticaDTO.setTirosIntentados(jsonObject.getDouble("fga"));
-                    estadisticaDTO.setTirosConvertidos(jsonObject.getDouble("fgm"));
-                    estadisticaDTO.setTirosLibresIntentados(jsonObject.getDouble("fta"));
-                    estadisticaDTO.setTirosLibresConvertidos(jsonObject.getDouble("ftm"));
-                    estadisticaDTO.setTriplesIntentados(jsonObject.getDouble("fg3a"));
-                    estadisticaDTO.setTriplesConvertidos(jsonObject.getDouble("fg3m"));
-                    estadisticaDTO.setRebotesPorPartido(jsonObject.getDouble("reb"));
-                    estadisticaDTO.setRebotesOfensivosPorPartido(jsonObject.getDouble("oreb"));
-                    estadisticaDTO.setRebotesDefensivosPorPartido(jsonObject.getDouble("dreb"));
-                    estadisticaDTO.setRobosPorPartido(jsonObject.getDouble("stl"));
-                    estadisticaDTO.setTaponesPorPartido(jsonObject.getDouble("blk"));
-                    estadisticaDTO.setPorcentajeTirosDeCampo(jsonObject.getDouble("fg_pct"));
-                    estadisticaDTO.setPorcentajeTriples(jsonObject.getDouble("fg3_pct"));
-                    estadisticaDTO.setPorcentajeTirosLibres(jsonObject.getDouble("ft_pct"));
-                    estadisticaDTO.setMinutosJugados(jsonObject.getString("min"));
-                    estadisticaDTO.setPartidosJugados(jsonObject.getInt("games_played"));
+                if (code != 200) {
+                    System.out.println("Se produjo un error: " + code);
+                    System.out.println(connection.getResponseMessage());
+                } else {
+                    StringBuilder information = new StringBuilder();
+                    Scanner sc = new Scanner(connection.getInputStream());
+
+                    while (sc.hasNext()) {
+                        information.append(sc.nextLine());
+                    }
+
+                    sc.close();
+
+                    JSONArray jsonArray = new JSONObject(information.toString()).getJSONArray("data");
+
+                    for (int j = 0; j < jsonArray.length(); j++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(j);
+                        EstadisticaDTO estadisticaDTO = new EstadisticaDTO();
+                        estadisticaDTO.setIdJugador(jsonObject.getInt("player_id"));
+                        estadisticaDTO.setPuntosPorPartido(jsonObject.getDouble("pts"));
+                        estadisticaDTO.setAsistenciasPorPartido(jsonObject.getDouble("ast"));
+                        estadisticaDTO.setPerdidasPorPartido(jsonObject.getDouble("turnover"));
+                        estadisticaDTO.setFaltasPorPartido(jsonObject.getDouble("pf"));
+                        estadisticaDTO.setTirosIntentados(jsonObject.getDouble("fga"));
+                        estadisticaDTO.setTirosConvertidos(jsonObject.getDouble("fgm"));
+                        estadisticaDTO.setTirosLibresIntentados(jsonObject.getDouble("fta"));
+                        estadisticaDTO.setTirosLibresConvertidos(jsonObject.getDouble("ftm"));
+                        estadisticaDTO.setTriplesIntentados(jsonObject.getDouble("fg3a"));
+                        estadisticaDTO.setTriplesConvertidos(jsonObject.getDouble("fg3m"));
+                        estadisticaDTO.setRebotesPorPartido(jsonObject.getDouble("reb"));
+                        estadisticaDTO.setRebotesOfensivosPorPartido(jsonObject.getDouble("oreb"));
+                        estadisticaDTO.setRebotesDefensivosPorPartido(jsonObject.getDouble("dreb"));
+                        estadisticaDTO.setRobosPorPartido(jsonObject.getDouble("stl"));
+                        estadisticaDTO.setTaponesPorPartido(jsonObject.getDouble("blk"));
+                        estadisticaDTO.setPorcentajeTirosDeCampo(jsonObject.getDouble("fg_pct"));
+                        estadisticaDTO.setPorcentajeTriples(jsonObject.getDouble("fg3_pct"));
+                        estadisticaDTO.setPorcentajeTirosLibres(jsonObject.getDouble("ft_pct"));
+                        estadisticaDTO.setMinutosJugados(jsonObject.getString("min"));
+                        estadisticaDTO.setPartidosJugados(jsonObject.getInt("games_played"));
 
 
-                    promedios.add(estadisticaDTO);
+                        promedios.add(estadisticaDTO);
+                    }
+                    System.out.println("ESTADISTICAS TODO BIEN");
                 }
-                System.out.println("ESTADISTICAS TODO BIEN");
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -104,11 +124,10 @@ public class EstadisticaServiceImpl implements EstadisticaService {
     }
 
     @Override
-    //@Scheduled(cron = "0 59 23 23 10 ?")
-    //@PostConstruct
+    @Scheduled(cron = "0 59 23 23 10 ?")
     public void insertarEstadisticas() {
         List<EstadisticaDTO> promedios = obtenerPromedios();
-        for (EstadisticaDTO estadisticaDTO: promedios) {
+        for (EstadisticaDTO estadisticaDTO : promedios) {
             estadisticaRepository.save(EstadisticaMapper.convertirAModelo(estadisticaDTO));
         }
     }
--- api/src/main/java/com/aulanosa/api/services/impl/JugadorServiceImpl.java
+++ api/src/main/java/com/aulanosa/api/services/impl/JugadorServiceImpl.java
@@ -37,7 +37,6 @@ public class JugadorServiceImpl implements JugadorService {
                 playerFinished = false;
                 cursor = 0;
                 while (!playerFinished) {
-                    //URL url = new URL("https://api.balldontlie.io/v1/players?team_ids[]=" + cont);
                     URL url = new URL("https://api.balldontlie.io/v1/players?team_ids[]=" + cont + "&per_page=100&cursor=" + cursor);
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestProperty("Authorization", System.getenv("BALL_DONT_LIE"));
@@ -61,46 +60,47 @@ public class JugadorServiceImpl implements JugadorService {
 
                         JSONArray jsonArray = new JSONObject(information.toString()).getJSONArray("data");
                         JSONObject meta = new JSONObject(information.toString()).getJSONObject("meta");
+
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            JugadorDTO jugadorDTO = new JugadorDTO();
+                            jugadorDTO.setIdJugador(jsonObject.getInt("id"));
+                            jugadorDTO.setIdEquipo(jsonObject.getJSONObject("team").getInt("id"));
+                            jugadorDTO.setNombreJugador(jsonObject.getString("first_name"));
+                            jugadorDTO.setApellidoJugador(jsonObject.getString("last_name"));
+                            jugadorDTO.setPosicionJugador(jsonObject.getString("position"));
+                            if (!jsonObject.isNull("height")) {
+                                jugadorDTO.setAlturaJugador(jsonObject.getString("height"));
+                            }
+
+                            if (!jsonObject.isNull("weight")) {
+                                jugadorDTO.setPesoJugador(jsonObject.optInt("weight", 0));
+                            }
+                            if (!jsonObject.isNull("jersey_number")) {
+                                jugadorDTO.setNumeroCamiseta(jsonObject.optInt("jersey_number", -1));
+                            }
+
+                            if (!jsonObject.isNull("college")) {
+                                jugadorDTO.setUniversidad(jsonObject.getString("college"));
+                            }
+
+                            if (!jsonObject.isNull("country")) {
+                                jugadorDTO.setProcedenciaJugador(jsonObject.getString("country"));
+                            }
+
+                            if (!jsonObject.isNull("draft_year")) {
+                                jugadorDTO.setAnioDraft(jsonObject.getInt("draft_year"));
+                            }
+                            if (!jsonObject.isNull("draft_round")) {
+                                jugadorDTO.setRondaDraft(jsonObject.getInt("draft_round"));
+                            }
+                            if (!jsonObject.isNull("draft_number")) {
+                                jugadorDTO.setNumeroDraft(jsonObject.getInt("draft_number"));
+                            }
+                            jugadores.add(jugadorDTO);
+                        }
                         if (meta.has("next_cursor")) {
                             cursor = meta.getInt("next_cursor");
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                JugadorDTO jugadorDTO = new JugadorDTO();
-                                jugadorDTO.setIdJugador(jsonObject.getInt("id"));
-                                jugadorDTO.setIdEquipo(jsonObject.getJSONObject("team").getInt("id"));
-                                jugadorDTO.setNombreJugador(jsonObject.getString("first_name"));
-                                jugadorDTO.setApellidoJugador(jsonObject.getString("last_name"));
-                                jugadorDTO.setPosicionJugador(jsonObject.getString("position"));
-                                if (!jsonObject.isNull("height")) {
-                                    jugadorDTO.setAlturaJugador(jsonObject.getString("height"));
-                                }
-
-                                if (!jsonObject.isNull("weight")) {
-                                    jugadorDTO.setPesoJugador(jsonObject.optInt("weight", 0));
-                                }
-                                if (!jsonObject.isNull("jersey_number")) {
-                                    jugadorDTO.setNumeroCamiseta(jsonObject.optInt("jersey_number", -1));
-                                }
-
-                                if (!jsonObject.isNull("college")) {
-                                    jugadorDTO.setUniversidad(jsonObject.getString("college"));
-                                }
-
-                                if (!jsonObject.isNull("country")) {
-                                    jugadorDTO.setProcedenciaJugador(jsonObject.getString("country"));
-                                }
-
-                                if (!jsonObject.isNull("draft_year")) {
-                                    jugadorDTO.setAnioDraft(jsonObject.getInt("draft_year"));
-                                }
-                                if (!jsonObject.isNull("draft_round")) {
-                                    jugadorDTO.setRondaDraft(jsonObject.getInt("draft_round"));
-                                }
-                                if (!jsonObject.isNull("draft_number")) {
-                                    jugadorDTO.setNumeroDraft(jsonObject.getInt("draft_number"));
-                                }
-                                jugadores.add(jugadorDTO);
-                            }
                         } else {
                             playerFinished = true;
                         }
@@ -109,7 +109,8 @@ public class JugadorServiceImpl implements JugadorService {
                 cont++;
             }
             System.out.println("TODO BIEN JUGADORES");
-        } catch (Exception e) {
+        } catch (
+                Exception e) {
             e.printStackTrace();
         }
 
@@ -117,8 +118,7 @@ public class JugadorServiceImpl implements JugadorService {
     }
 
     @Override
-    //@Scheduled(cron = "0 59 23 23 10 ?")
-    @PostConstruct
+    @Scheduled(cron = "0 59 23 23 10 ?")
     public void almacenarJugadores() {
 
         List<JugadorDTO> jugadores = insertarJugadores();
